2fde45952c22d91a6c551c488758ba599acd6fb2
Vitalii BONDARENKO <vibondar@e2r15p4.unit.ua>
2018-10-04 21:54:40 +0300

    search in js

--- a_search/static/search/search.js
+++ a_search/static/search/search.js
@@ -10,7 +10,21 @@ $('#search_form').on('submit', function(e) {
 			csrfmiddlewaretoken: document.getElementsByName('csrfmiddlewaretoken')[0].value,
 		},
     	success: function(response){
-    		console.log(response);
+            console.log(response);
+    		if(response['data']['movie_count'] > 0)
+    		{
+    		       console.log($(".search_row").html());
+                   let i = 0;
+                    $( ".search_row").html('<th class="search_item"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+                    i++;
+                    while(i < response['data']['movie_count'])
+                    {
+                        $( ".search_row").html($( ".search_row").html() + '<th class="search_item"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+                        i++;
+                    }
+    		}else{
+    		     console.log("kek\n");
+    		}
 
 		}
 	});
