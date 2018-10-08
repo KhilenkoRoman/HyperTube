538cd1e4e632e4a2f03f678b46fa9fc989f96ae0
Vitalii BONDARENKO <vibondar@e3r6p1.unit.ua>
2018-10-08 17:20:28 +0300

    ul li search

--- a_search/static/search/search.js
+++ a_search/static/search/search.js
@@ -13,19 +13,17 @@ $('#search_form').on('submit', function(e) {
             console.log(response);
     		if(response['data']['movie_count'] > 0)
     		{
-    		       console.log($(".search_row").html());
-                   let i = 0;
-                    $( ".search_row").html('<th class="search_item"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+    		    let i = 0;
+    		    $( ".result").html('<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+    		    i++;
+    		    while(i < response['data']['movie_count'])
+                {
+                    $( ".result").html($( ".result").html() + '<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
                     i++;
-                    while(i < response['data']['movie_count'])
-                    {
-                        $( ".search_row").html($( ".search_row").html() + '<th class="search_item"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
-                        i++;
-                    }
+                }
     		}else{
-    		     console.log("kek\n");
+   		     console.log("kek\n");
     		}
-
-		}
+    	}
 	});
 });
