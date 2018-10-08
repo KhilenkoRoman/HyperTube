4bddfc9a28cde8e6d51443deb05439359073b3ed
Vitalii BONDARENKO <vibondar@e3r6p1.unit.ua>
2018-10-08 18:00:39 +0300

    lolkek

--- a_search/static/search/search.js
+++ a_search/static/search/search.js
@@ -13,17 +13,18 @@ $('#search_form').on('submit', function(e) {
             console.log(response);
     		if(response['data']['movie_count'] > 0)
     		{
-    		    let i = 0;
-    		    $( ".result").html('<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
-    		    i++;
-    		    while(i < response['data']['movie_count'])
-                {
-                    $( ".result").html($( ".result").html() + '<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
-                    i++;
-                }
+                  let i = 0;
+                   $( ".result").html('<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+                   i++;
+                   while(i < response['data']['movie_count'] && i < response['data']['limit'])
+                   {
+                       $( ".result").html($( ".result").html() + '<li class="results-item-wrap"> <a href=""> <div> <img src="' + response['data']['movies'][i]['medium_cover_image'] + '" class="poster"></div><p>' + response['data']['movies'][i]['title_english'] + '</p><p>' + response['data']['movies'][i]['genres'] + '</p></a></th>');
+                       i++;
+                   }
     		}else{
    		     console.log("kek\n");
-    		}
-    	}
+   		}
+
+		}
 	});
 });
